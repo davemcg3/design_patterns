eb75ca186dc10dbb640d5dacb45526a97a129bf6
David McGrath <mcgrath@mobilecause.com>
2017-12-12 08:31:58 -0800

    Tweaks to real facade example

--- real/facade/ruby/adpicker.rb
+++ real/facade/ruby/adpicker.rb
@@ -95,7 +95,7 @@ end
 ###############################################################################
 
 class AbstractAdRotationStrategy
-  def initialize (count = nil, adfree = false)
+  def initialize (count = nil)
     @count = count unless count.nil?
   end
 
@@ -112,7 +112,7 @@ end
 
 class DecisonMethodRoundRobin < AbstractAdRotationStrategy
   def decide
-    raise "Unknown network count" if @count.nil?
+    raise "Unknown network count" if @count.nil? # would need better error checking for a production app
     # would need to slam this into something like redis on a website so it was available on new requests
     @last = @last || -1 # initialize
     @last = @last + 1 # increment
@@ -125,22 +125,27 @@ end
 class AdNetworkRegistry
   attr_reader :rotationStrategy
 
-  def initialize (rotationStrategy="randomized", adfree=false)
+  def initialize (rotationStrategy = "round robin", adfree = false)
     setStrategy rotationStrategy, adfree
   end
 
-  def setStrategy (rotationStrategy="randomized", adfree=false)
+  def setStrategy (rotationStrategy = "round robin", adfree = false)
     case rotationStrategy
-    when "round robin"
-      networks = ['Google', 'Facebook', 'Amazon']
-      networks << "Abstract" if adfree
-      networks.each do |network|
-        net = Object::const_get(network + 'Ad').new
-        net.reset_displayed
-      end
-      @rotationStrategy = DecisonMethodRoundRobin.new self.count, true
-    else
-      @rotationStrategy = DecisionMethodTimeRandom.new
+      when "randomized"
+        # networks are lazy-loaded
+        @rotationStrategy = DecisionMethodTimeRandom.new
+      else
+        networks = ['Google', 'Facebook', 'Amazon']
+        networks << "Abstract" if adfree # need something to instantiate so can't use "booga booga" here
+        # need the network count to instantiate the decision strategy so eager load networks
+        networks.each do |network|
+          Object::const_get(network + 'Ad').new
+        end
+        @rotationStrategy = DecisonMethodRoundRobin.new self.count
+    end
+
+    self.networks.each do |network|
+      network.reset_displayed
     end
   end
 
@@ -158,22 +163,22 @@ class AdNetworkRegistry
     self.networks.each do |network|
       networkSummary[network.to_s] = network.displayed
     end
-    summary + networkSummary.map{|k,v| "#{k}: #{v}"}.join(', ')
+    summary + networkSummary.map {|k, v| "#{k}: #{v}"}.join(', ')
   end
 
   def getAd
     adDisplayer = AdDisplayer.new
     adContent = case @rotationStrategy.decide
-    when 0
-      adDisplayer.display('Google')
-    when 1
-      adDisplayer.display('Facebook')
-    when 2
-      adDisplayer.display('Amazon')
-    else
-      # this is obviously a sub-par way to handle the else case, but I wanted to show that you can pass anything to the
-      # AdDisplayer and it can handle the input sensibly
-      adDisplayer.display('booga booga')
+      when 0
+        adDisplayer.display('Google')
+      when 1
+        adDisplayer.display('Facebook')
+      when 2
+        adDisplayer.display('Amazon')
+      else
+        # this is obviously a sub-par way to handle the else case, but I wanted to show that you can pass anything to the
+        # AdDisplayer and it can handle the input sensibly
+        adDisplayer.display('booga booga')
     end
 
     adContent
@@ -182,25 +187,26 @@ end
 
 ###############################################################################
 
-def simulateAdsBeingShown registry, testCases=1
-  print "#{testCases} ads being shown using the #{registry.rotationStrategy.class} Strategy\n"
+def simulateAdsBeingShown registry, testCases = 1
+  print "\n" + "="*24
+  print "\n#{testCases} ads being shown using the #{registry.rotationStrategy.class} Strategy\n"
   testCases.times {
-    p registry.getAd
+    print registry.getAd + "\n"
     # simulate requests coming in at different times to generate different ads on a randomized strategy
     sleep(rand(10) / 10.0) if registry.rotationStrategy.class == DecisionMethodTimeRandom # sleeping for a random amount of milliseconds before rerunning
   }
+  print registry.networkSummary + "\n"
+  print "="*24 + "\n"
 end
 
 def run
-  testCases = 5
+  testCases = 30
 
   registry = AdNetworkRegistry.new
   simulateAdsBeingShown registry, testCases
-  print registry.networkSummary + "\n"
 
-  registry.setStrategy "round robin", true
+  registry.setStrategy "randomized", true
   simulateAdsBeingShown registry, testCases
-  print registry.networkSummary + "\n"
 
 end
 
