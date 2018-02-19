cdcac8e08b3176579681e55ab436f490f93a70c1
David McGrath <dave.mcgrath3@gmail.com>
2018-02-19 09:51:21 +0000

    Changed blockchain endpoint and network hit

--- real/chain_of_responsibility/javascript/index.js
+++ real/chain_of_responsibility/javascript/index.js
@@ -14,7 +14,8 @@ if (typeof(web3) !== typeof(undefined)) {
   web3 = new Web3(web3.currentProvider);
 } else {
   // set the provider you want from Web3.providers
-  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  // web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/81cxtvxXTMlKktWXNJjb"));
   // web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:30303")); // https://github.com/ethereum/go-ethereum/issues/2982#issuecomment-245296183
 }
 
